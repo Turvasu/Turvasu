f7c875f8b0c07177f49496d9d12598beea46b528
Turvasu <jaiswalturvasu@gmail.com>
2025-09-23 23:33:21 +0530

    Time: 1 ms (99.44%) | Memory: 42.1 MB (93.72%) - LeetSync

---
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+class Solution {
+    public int maxFrequencyElements(int[] n) {
+        int c=1,b=1,f=1;
+        Arrays.sort(n);
+        for(int i=0;i<n.length-1;i++)
+        {
+            if(n[i]==n[i+1])
+                c++;
+            else
+            {
+                if(b==c)
+                    f=b+f;
+                else if(c>b)
+                {
+                    b=c;
+                    f=b;
+                }
+                c=1;   
+            }
+            if(n[i]==n[i+1] && i==n.length-2)
+            {
+                if(b==c)
+                    f=b+f;
+                else if(c>b)
+                {
+                    b=c;
+                    f=b;
+                }
+            }
+        }
+        return f;
+    }
+}
