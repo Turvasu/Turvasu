89c00c320ea9bce36b11ffe2ef6e358c50ba6558
Turvasu <jaiswalturvasu@gmail.com>
2025-09-27 00:29:59 +0530

    Time: 2 ms (10.51%) | Memory: 43.4 MB (80.13%) - LeetSync

---
+++ 3998-minimum-operations-to-equalize-array/minimum-operations-to-equalize-array.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+class Solution {
+    public int minOperations(int[] nums) {
+        Arrays.sort(nums);
+        if(nums[0]==nums[nums.length-1])
+            return 0;
+        return 1;
+    }
+}
